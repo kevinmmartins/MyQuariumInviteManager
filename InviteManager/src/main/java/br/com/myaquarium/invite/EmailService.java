4decfaf8288989e9eca9b87dd62f2bc666e91ad8
Kevin Martins <kevinmmartins@gmail.com>
2017-09-09 20:53:24 -0300

    First Commit

---
+++ InviteManager/src/main/java/br/com/myaquarium/invite/EmailService.java
@@ -0,0 +1,34 @@
+package br.com.myaquarium.invite;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+	public void send(String name, String toEmail) {
+
+		try {
+			Email email = new SimpleEmail();
+			email.setHostName("smtp.googlemail.com");
+			email.setSmtpPort(465);
+			email.setAuthenticator(new DefaultAuthenticator("myquariumteam@gmail.com", "uzumakikevin"));
+			email.setSSLOnConnect(true);
+
+			email.setFrom("myquariumteam@gmail.com");
+			email.setSubject("Bem vindo ao MyQuarium !");
+			email.setMsg("Olá " + name
+					+ ". Esperamos que você tenha uma ótima experiencia com o nosso serviço. Qualquer problema não deixe de nos informar myquariumteam@gmail.com");
+			email.addTo(toEmail);
+			email.send();
+
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
